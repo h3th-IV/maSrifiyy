e9c51b62cc58b507495ad0722c070b7782805c20
samuel bonu <samuelbonux10@gmail.com>
2025-01-22 22:06:10 +0100

    db ops

--- db/db.go
+++ db/db.go
@@ -23,7 +23,6 @@ type PostgresStore struct {
 }
 
 func NewPostgreStore() (*PostgresStore, error) {
-	//created new sqlDB, then decide to port that to gormDB
 	connStr := "user=heth dbname=masrifiyy password=yourpassword host=172.17.0.2 port=5432 sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
@@ -41,29 +40,83 @@ func NewPostgreStore() (*PostgresStore, error) {
 }
 
 func (s *PostgresStore) CreateUserAccount(user *models.Sellers) (bool, error) {
+	query := `INSERT INTO sellers (first_name, last_name, email, password, created_at) 
+	VALUES ($1, $2, $3, $4, NOW())`
+	_, err := s.db.Exec(query, user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		log.Printf("Error creating user account: %v", err)
+		return false, err
+	}
 	return true, nil
 }
 
-func (s *PostgresStore) UpdateUserAccount(*models.Sellers) (bool, error) {
+func (s *PostgresStore) UpdateUserAccount(user *models.Sellers) (bool, error) {
+	query := `UPDATE sellers SET first_name = $1, last_name = $2, email = $3, password = $4 
+	WHERE id = $5`
+	_, err := s.db.Exec(query, user.FirstName, user.LastName, user.Email, user.Password, user.ID)
+	if err != nil {
+		log.Printf("Error updating user account: %v", err)
+		return false, err
+	}
 	return true, nil
 }
 
 func (s *PostgresStore) GetUserAccountById(id int) (*models.Sellers, error) {
-	return nil, nil
+	query := `SELECT id, first_name, last_name, email, password, created_at FROM sellers WHERE id = $1`
+	row := s.db.QueryRow(query, id)
+	var user models.Sellers
+	err := row.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.CreatedAt)
+	if err != nil {
+		if err == sql.ErrNoRows {
+		}
+		log.Printf("Error fetching user account by ID: %v", err)
+		return nil, err
+	}
+	return &user, nil
 }
 
-func (s *PostgresStore) AddItem(*models.Goods, *models.Sellers) (bool, error) {
+func (s *PostgresStore) AddItem(item *models.Goods, user *models.Sellers) (bool, error) {
+	query := `INSERT INTO goods (name, quantity, max_threshold, created_by) 
+	VALUES ($1, $2, $3, $4)`
+	_, err := s.db.Exec(query, item.Name, item.Quantity, item.MaxThreshold, user.ID)
+	if err != nil {
+		log.Printf("Error adding item: %v", err)
+		return false, err
+	}
 	return true, nil
 }
 
-func (s *PostgresStore) UpdateItem(*models.Goods, *models.Sellers) (bool, error) {
+func (s *PostgresStore) UpdateItem(item *models.Goods, user *models.Sellers) (bool, error) {
+	query := `UPDATE goods SET name = $1, quantity = $2, max_threshold = $3 
+	WHERE id = $4 AND created_by = $5`
+	_, err := s.db.Exec(query, item.Name, item.Quantity, item.MaxThreshold, item.ID, user.ID)
+	if err != nil {
+		log.Printf("Error updating item: %v", err)
+		return false, err
+	}
 	return true, nil
 }
 
-func (s *PostgresStore) SetItemMaxThreshold(*models.Goods, *models.Sellers) (bool, error) {
+func (s *PostgresStore) SetItemMaxThreshold(item *models.Goods, user *models.Sellers) (bool, error) {
+	query := `UPDATE goods SET max_threshold = $1 WHERE id = $2 AND created_by = $3`
+	_, err := s.db.Exec(query, item.MaxThreshold, item.ID, user.ID)
+	if err != nil {
+		log.Printf("Error setting item max threshold: %v", err)
+		return false, err
+	}
 	return true, nil
 }
 
-func (s *PostgresStore) GetItemById(int) (*models.Goods, error) {
-	return nil, nil
+func (s *PostgresStore) GetItemById(id int) (*models.Goods, error) {
+	query := `SELECT id, name, quantity, max_threshold, created_by FROM goods WHERE id = $1`
+	row := s.db.QueryRow(query, id)
+	var item models.Goods
+	err := row.Scan(&item.ID, &item.Name, &item.Quantity, &item.MaxThreshold, &item.CreatedBy)
+	if err != nil {
+		if err == sql.ErrNoRows {
+		}
+		log.Printf("Error fetching item by ID: %v", err)
+		return nil, err
+	}
+	return &item, nil
 }
